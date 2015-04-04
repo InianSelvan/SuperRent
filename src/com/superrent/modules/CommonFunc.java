2f1d3c6d5acccb6f1d288677cbf1480278c578aa
InianSelvan <iniyan.selvan@gmail.com>
2015-04-04 00:49:16 -0700

    Inian -pulling

--- src/com/superrent/modules/CommonFunc.java
+++ src/com/superrent/modules/CommonFunc.java
@@ -6,26 +6,42 @@
 package com.superrent.modules;
 
 import com.toedter.calendar.JDateChooser;
+
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Properties;
+import javax.mail.Session;
 import javax.swing.JSpinner;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.joda.time.DateTime;
 
 /**
  *
  * @author Iniyan
  */
 public class CommonFunc {
-    public static String username;//Yaoyao added
-    
     public static Date changeDateFormat(JDateChooser jdate) throws ParseException{
         Date dt = jdate.getDate();
         java.sql.Date sqldate = new java.sql.Date(dt.getTime());
         return sqldate;
     }
-    
+    public static int compareDates(Date first, Date second){
+        DateTime firstdt = new DateTime(first);
+        Date fdt = firstdt.toLocalDate().toDate();
+        DateTime seconddt = new DateTime(second);
+        Date sdt = seconddt.toLocalDate().toDate();        
+       return fdt.compareTo(sdt);
+    }
     public static String sqlTime(JSpinner HH, JSpinner MM){
         String hh = String.valueOf(HH.getValue());
         String mm = String.valueOf(MM.getValue());
@@ -40,4 +56,55 @@ public class CommonFunc {
         //System.out.println(MyDate);
         return ResultDate;
     }
+    
+     public static void triggerMail(String toPerson, String Subject, String Msg) {
+      // Recipient's email ID needs to be mentioned.
+      String to = toPerson;
+      String from = "superrentt@gmail.com";
+      final String username = "superrentt";
+      final String password = "SuperRent123";
+
+      
+      String host = "smtp.gmail.com";
+
+      Properties props = new Properties();
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.smtp.host", host);
+      props.put("mail.smtp.port", "587");
+
+      
+      Session session = Session.getInstance(props,
+      new javax.mail.Authenticator() {
+         protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication(username, password);
+         }
+      });
+
+      try {
+         // Create a default MimeMessage object.
+         Message message = new MimeMessage(session);
+
+         // Set From: header field of the header.
+         message.setFrom(new InternetAddress(from));
+
+         // Set To: header field of the header.
+         message.setRecipients(Message.RecipientType.TO,
+         InternetAddress.parse(to));
+
+         // Set Subject: header field
+         message.setSubject(Subject);
+
+         // Now set the actual message
+         message.setText(Msg);
+
+         // Send message
+         Transport.send(message);
+
+         System.out.println("Sent message successfully....");
+
+      } catch (MessagingException e) {
+            throw new RuntimeException(e);
+      }
+   }
 }
--- src/com/superrent/modules/Reserve.java
+++ src/com/superrent/modules/Reserve.java
@@ -5,10 +5,183 @@
  */
 package com.superrent.modules;
 
+import com.superrent.DataBase.ConnectDB;
+import com.superrent.gui.SuperRent;
+import java.io.IOException;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
- * @author yaoyaolin
+ * @author Iniyan
  */
-public class Reserve extends Rent {
+public class Reserve {
+     
+    public void fillBranchIDCombo(JComboBox branchCombo) throws ClassNotFoundException, SQLException, IOException{
+        try{ 
+            ConnectDB.exeQuery("SELECT branch_id FROM branch");
+        
+            while(ConnectDB.resultSet().next()){
+                int branchId = Integer.parseInt(ConnectDB.resultSet().getString("branch_id"));
+                branchCombo.addItem(branchId);
+             }
+            
+        }
+        finally{
+            ConnectDB.clearResultSet();
+        }
+    } 
+   
+    public void fillVehicleCatCombo(JComboBox vehicleCatCombo, JComboBox branchCombo, JComboBox vehicleType) throws ClassNotFoundException, SQLException, IOException{
+        if(!branchCombo.getSelectedItem().toString().equals("Select")){
+            int branchId = Integer.parseInt(branchCombo.getSelectedItem().toString());
+            String VehicleType = vehicleType.getSelectedItem().toString();
+            try{
+                ConnectDB.exeQuery("SELECT category FROM fleet WHERE branch_id = "+branchId+" and car_or_truck ='"+VehicleType+"'");
+                while(ConnectDB.resultSet().next()){
+                    String carType = ConnectDB.resultSet().getString("category");
+                    vehicleCatCombo.addItem(carType);
+                }
+            }finally{
+                ConnectDB.clearResultSet();
+            }
+        }
+    }
+    
+        
+    
+    public DefaultListModel fillEquipmentList(JList equipments, JComboBox cartypeCombo, JComboBox branchid ) throws ClassNotFoundException, SQLException, IOException, InterruptedException{
+
+        DefaultListModel listEquip= new DefaultListModel();
+        try{
+            String vehicalType = null;
+            
+            String branch_id = branchid.getSelectedItem().toString();
+            if(cartypeCombo.getSelectedItem()!=null && branchid.getSelectedItem()!="Select")
+            {
+                vehicalType = cartypeCombo.getSelectedItem().toString();
+            }        
+            ConnectDB.exeQuery("SELECT equipment_type from equipment where car_or_truck='"+vehicalType+"' and branch_id='"+branch_id+"'");
+            while(ConnectDB.resultSet().next()){
+                String EquipType = ConnectDB.resultSet().getString("equipment_type");
+                listEquip.addElement(EquipType);
+            }
+        }finally{
+            ConnectDB.clearResultSet();
+        }
+        
+        return listEquip;
+    }
+    
+    public int validateCustomerInfo(JTextField cusNumField) throws ClassNotFoundException, SQLException, IOException{
+        int cusID =0;
+        try{
+            String phoneNum = cusNumField.getText();       
+            ConnectDB.exeQuery("SELECT customer_id FROM customer WHERE phone ='"+phoneNum+"'");
+            
+            if(ConnectDB.resultSet().next()){
+                cusID = Integer.parseInt(ConnectDB.resultSet().getString("customer_id"));
+            }
+        }finally{    
+            ConnectDB.clearResultSet();
+            return cusID;
+        }
+    }
+        
+    public static long genUniqueID(){
+        Calendar now = Calendar.getInstance();
+        int year = now.get(Calendar.YEAR);
+        int month = now.get(Calendar.MONTH)+1;
+        int date = now.get(Calendar.DATE);
+        int hour = now.get(Calendar.HOUR);
+        int min = now.get(Calendar.MINUTE);
+        int sec = now.get(Calendar.SECOND);
+        String random = ""+year+month+date+hour+min+sec;
+        return Long.valueOf(random);
+    }
+
+    public DefaultTableModel getAvailableVehicles(String vehicleCat, int branchId, String VehicleType) throws ClassNotFoundException, SQLException, IOException{
+        int num = countAvailableVehicles(vehicleCat, branchId, VehicleType);
+        String[][] VehicleInfo=null;
+        String[] col = new String[5];
+        
+        try{
+            ConnectDB.exeQuery("select v.vin, v.category, v.color, v.doors, v.branch_id  from fleet as v where vin not in\n" +
+                               "(select vin from reserve where status = 'reserved' or status = 'rented' ) and branch_id ='"+branchId+"' and category = '"+vehicleCat+"' and car_or_truck = '"+VehicleType+"'");
+
+        
+        ResultSetMetaData md = ConnectDB.resultSet().getMetaData();
+        
+        col[0] = md.getColumnName(1);
+        col[1] = md.getColumnName(2);
+        col[2] = md.getColumnName(3);
+        col[3] = md.getColumnName(4);
+        col[4] = md.getColumnName(5);
+            
+        VehicleInfo= new String[num][5];
+            for(int i=0; ConnectDB.resultSet().next(); i++){
+                for(int j=1; j<=5; j++){
+                    VehicleInfo[i][j-1]= ConnectDB.resultSet().getString(j); 
+                }
+            }        
+        }
+        finally{
+            ConnectDB.clearResultSet();  
+        }
+        return new DefaultTableModel(VehicleInfo, col); 
+    }
+    
+    public  int countAvailableVehicles(String vehicleCat, int branchId, String VehicleType) throws ClassNotFoundException, SQLException, IOException{
+        int countVehi=0;
+        try{
+            ConnectDB.exeQuery("select count(v.vin)  from fleet as v where vin not in\n" +
+                               "(select vin from reserve where status = 'reserved' or status = 'rented' ) and branch_id ='"+branchId+"' and category = '"+vehicleCat+"' and car_or_truck = '"+VehicleType+"'");
+           while(ConnectDB.resultSet().next()){
+               countVehi = Integer.parseInt(ConnectDB.resultSet().getString(1));
+           }
+        }finally{
+            ConnectDB.clearResultSet();
+        }
+        return countVehi;
+    }
+    
+    public void cancelReservation(JTextField confirmationNo){
+        int count = 0;
+        try {
+                ConnectDB.exeQuery("SELECT Count(confirmation_no) FROM reserve WHERE confirmation_no ="+Integer.parseInt(confirmationNo.getText().toString()));
+                ConnectDB.clearResultSet();
+                while(ConnectDB.resultSet().next()){
+                    count = Integer.parseInt(ConnectDB.resultSet().getString(1));
+                }
+                if(count>0){
+                    ConnectDB.exeUpdate("DELETE FROM reserve WHERE confirmation_no ="+Integer.parseInt(confirmationNo.getText().toString()));
+                    ConnectDB.clearResultSet();
+                    ConnectDB.exeUpdate("DELETE FROM equipment_reserved WHERE confirmation_no = "+Integer.parseInt(confirmationNo.getText().toString()) );
+                    ConnectDB.clearResultSet();
+                    JOptionPane.showMessageDialog(null, "Reservation cancelled for "+confirmationNo.getText().toString());
+                }else{
+                    JOptionPane.showMessageDialog(null, "Not a valid confirmation_no");
+                }
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, ex);
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex);
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(null, ex);
+            } catch (NumberFormatException e){
+                JOptionPane.showMessageDialog(null, e);
+                
+            }
+    }
     
 }
